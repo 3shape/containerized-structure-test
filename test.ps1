ee63d3a3f25be303b2763c9e1e1a3390cc6430eb
Joseph Petersen <joseph.petersen@3shape.com>
2019-11-18 17:49:15 +0100

    add appveyor to support multi platform of docker build

---
+++ test.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+if ($env:ARCH -ne "amd64") {
+  Write-Host "Arch $env:ARCH detected. Skip testing."
+  exit 0
+}
+
+Write-Host Starting test
+
+docker kill containertest-test
+docker rm -f containertest-test
+
+$ErrorActionPreference = 'Stop';
+
+if ($IsWindows) {
+  docker pull mcr.microsoft.com/windows/nanoserver:1809
+  docker run --name containertest-test --rm -v "${PWD}:C:/configs" -v \\.\pipe\docker_engine:\\.\pipe\docker_engine containertest test -c /configs/gc-windows-config.yml -i mcr.microsoft.com/windows/nanoserver:1809
+} else {
+  docker pull ubuntu:18.04
+  docker run --name containertest-test --rm -v "${PWD}:/configs" -v /var/run/docker.sock:/var/run/docker.sock containertest test -c /configs/gc-linux-config.yml -i ubuntu:18.04
+}
