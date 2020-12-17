431e737adb6f26487e1f4a0c7b6ccf196520292b
Khang Yeen Lee <KhangYeen.Lee@3shape.com>
2020-12-17 12:48:37 +0100

    Reorg.

--- build.ps1
+++ build.ps1
@@ -4,4 +4,4 @@ Write-Host Starting build
 
 docker info
 $os = If ($isWindows) {'windows'} Else {'linux'}
-docker build --tag containertest --file "${os}.dockerfile" .
+docker build --tag containertest --file "src/${os}.dockerfile" .
